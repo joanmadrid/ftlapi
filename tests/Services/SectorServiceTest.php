ead567e5fe67af8d01756c4a598578825ce774ef
Joan Madrid <joanmadrid@gmail.com>
2018-05-23 19:48:04 +0200

    Add entity service + phpunit dist whitelist

---
+++ tests/Services/SectorServiceTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Tests;
+
+use App\Entity\Sector;
+use App\Entity\Universe;
+use App\Services\SectorService;
+use PHPUnit\Framework\TestCase;
+
+class SectorServiceTest extends TestCase
+{
+    /**
+     * @covers SectorService::generate()
+     * @throws \Exception
+     */
+    public function testGenerateSuccess()
+    {
+        $sectorService = new SectorService();
+
+        /** @var Universe $universe */
+        $universe = new Universe();
+        $universe->setName('test-universe');
+
+        $root = $sectorService->generate($universe);
+
+        $expectedRoot = new Sector();
+        $expectedRoot->setUniverse($universe);
+
+        $this->assertEquals($root, $expectedRoot);
+    }
+}
