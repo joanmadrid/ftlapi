5d912b3f961fc57f58822dd677709e7623ad5268
Noé Andrés <nandrmarc@gmail.com>
2018-05-28 19:13:58 +0000

    temporal commit

--- tests/Services/ShipAttributesServiceTest.php
+++ tests/Services/ShipAttributesServiceTest.php
@@ -5,7 +5,9 @@ namespace App\Tests;
 
 use App\Entity\Ship;
 use App\Entity\ShipAttribute;
+use App\Entity\ShipAttributeType;
 use App\Kernel;
+use App\Services\ShipAttributeService;
 use App\Services\ShipAttributeTypeService;
 use App\Services\ShipService;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
@@ -15,10 +17,27 @@ class ShipAttributesServiceTest extends AppKernelTest
     /**
      * @param ShipService $shipService
      * @throws \Exception
-     * @covers \App\Services\ShipService::create()
+     * @covers \App\Services\ShipAttributeService::addAttributes()
      */
     public function testCreateSuccess()
     {
-        
+        /** @var ShipAttributeTypeService $shipAttributeTypeService */
+        $shipAttributeTypeService = $this->container->get(ShipAttributeTypeService::class);
+        $allTypes = $shipAttributeTypeService->getRepository()->findAll();
+        $shipAttributesArray = [];
+
+        /** @var ShipAttributeType $type */
+        foreach ($allTypes as $type) {
+            $shipAttributesArray[$type->getId()] = [
+                'name' => $type->getName(),
+                'level' => 0
+            ];
+        }
+
+        /** @var Ship $ship */
+        $ship = new Ship();
+        /** @var ShipAttributeService $shipAttributeService */
+        $shipAttributeService = $this->container->get(ShipAttributeService::class);
+        $shipAttributeService->addAttributes($ship, $shipAttributesArray);
     }
 }
